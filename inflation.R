b691b7d084e43cb2f136287f75bd28dbd89e9915
JLiekenbrock <69990358+JLiekenbrock@users.noreply.github.com>
2022-06-06 10:23:39 +0200

    add gdp and calculate inflation monthly

--- inflation.R
+++ inflation.R
@@ -7,16 +7,42 @@ library(plotly)
 library(countrycode)
 library(ggthemes)
 #library(dtwclust)
+library(zoo)
 
 #data("uciCT")
-
 
 #deflation <- read_excel("Inflation-data.xlsx",sheet="def_a")
 
 inflation <- read_excel("Inflation-data.xlsx",sheet="ccpi_a")
 
+gdp = read_csv("gdp.csv")%>%
+  mutate(region = countrycode(sourcevar = .$LOCATION,
+              origin = "iso3c",
+              destination = "region"))%>%
+  filter(FREQUENCY=="Q")%>%
+  mutate(date = as.yearqtr(TIME,format = "%Y-Q%q"))%>%
+  group_by(region,date)%>%
+  summarise(value=median(Value,na.rm=T))%>%
+  filter(date > as.yearqtr("2006-Q1",format = "%Y-Q%q"))
+
+g = ggplot()+
+  geom_line(gdp,mapping=aes(date,value,group=region,color=region))+
+  scale_color_tableau()+
+  theme_igray()
+g
+ggsave("gdp.png",width=12,height=12)
+ggplotly(g)
+
+inflation <- read_excel("Inflation-data.xlsx",sheet="ccpi_m")
+
 data = inflation%>%
   pivot_longer(!c(1:5),names_to="Year",values_to="value")
+
+data = data%>%
+  group_by(Country)%>%
+  mutate(date = as.Date(Year, format = "%Y%M"))%>%
+  mutate(value = (value/lag(value,12)-1)*100)%>%
+  ungroup()
 
 data$region=countrycode(sourcevar = data$`Country Code`,
             origin = "wb",
@@ -24,25 +50,32 @@ data$region=countrycode(sourcevar = data$`Country Code`,
 
 data = data%>%
   mutate(`IMF Country Code` = as.character(`IMF Country Code`))%>%
-  group_by(region,Year,`Indicator Type`)%>%
+  group_by(region,date,`Indicator Type`)%>%
   summarise(value = median(value,na.rm=T))
 
 data = data%>%
-  mutate(Year = as.integer(Year))%>%
-  filter(Year > 2000)
+  #mutate(Year = as.integer(Year))%>%
+  filter(date > as.Date("2005",format="%Y"))
+  # rowwise()%>%
+  # mutate(value = pmin(value,10))
 
 max(data$value,na.rm=T)
+
 g = data%>%
   ggplot()+
-    geom_rect(xmin=2007,xmax=2009,ymin=0,ymax=max(data$value,na.rm=T),fill="grey")+
-    geom_rect(xmin=2010,xmax=2014,ymin=0,ymax=max(data$value,na.rm=T),fill="grey")+
-    geom_rect(xmin=2020,xmax=2022,ymin=0,ymax=max(data$value,na.rm=T),fill="grey")+
+    geom_rect(xmin=as.Date("2007",format="%Y"),xmax=as.Date("2009",format="%Y"),ymin=0,ymax=max(data$value,na.rm=T),fill="grey")+
+    geom_rect(xmin=as.Date("2010",format="%Y"),xmax=as.Date("2014",format="%Y"),ymin=0,ymax=max(data$value,na.rm=T),fill="grey")+
+    geom_rect(xmin=as.Date("2020",format="%Y"),xmax=as.Date("2022",format="%Y"),ymin=0,ymax=max(data$value,na.rm=T),fill="grey")+
+    geom_label(x= as.Date("2008",format="%Y"), y = 10,label="financial crisis")+
+    geom_label(x= as.Date("2012",format="%Y"), y = 10,label="European debt crisic")+
+    geom_label(x= as.Date("2021",format="%Y"), y = 10,label="Covid-19")+
     geom_hline(yintercept = 2,color="red",size=2)+
-    geom_line(aes(Year,value,group=region,color=region),size=1)+
-    annotate("rect",xmin=min(data$Year,na.rm=T),xmax=max(data$Year,na.rm=T),ymin=0,ymax=2,fill="grey",alpha=0.6)+
+    geom_line(aes(date,value,group=region,color=region),size=1)+
+    #annotate("rect",xmin=min(data$Year,na.rm=T),xmax=max(data$Year,na.rm=T),ymin=0,ymax=2,fill="grey",alpha=0.6)+
     scale_color_tableau()+
     theme_igray()
 
-
 g
+
+ggsave("inflation.png",height=12,width=12)
 ggplotly(g)
